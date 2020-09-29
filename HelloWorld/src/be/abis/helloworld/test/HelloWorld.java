912e7f15d730fa88279fb093bedc66c29d3c6830
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-09-29 09:43:38 +0200

    HelloWorld

---
+++ HelloWorld/src/be/abis/helloworld/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package be.abis.helloworld.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello " + args[0]);
+		//System.out.println("Hello");
+		System
+	}
+
+}
